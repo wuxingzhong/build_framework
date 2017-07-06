f64562319369179c406bf90e3735734a9de13de8
wuxingzhong <330332812@qq.com>
2017-07-06 18:32:42 +0800

    add c and cpp build framework

---
+++ c_cpp/test/test.c
@@ -0,0 +1,15 @@
+/*
+* @Author: wuxingzhong
+* @Date:   2017-07-06 15:07:03
+* @Email:  330332812@qq.com
+* @copyright:          sunniwell
+* @Last Modified by:   wuxingzhong
+* @Last Modified time: 2017-07-06 15:07:16
+*/
+
+#include <stdio.h>
+
+int test() {
+
+    return 0;
+}
